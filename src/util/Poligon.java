34f27553ecdfdf01c3999f0ed8d917dc84a6d0ba
m-doru <doru.musuroi@gmail.com>
2015-11-28 13:31:07 +0200

    Cleared classes

--- src/util/Poligon.java
+++ src/util/Poligon.java
@@ -1,18 +1,5 @@
 package util;
 
 public class Poligon {
-	int a;
-	Poligon(int a){
-		this.a = a;
-	}
-	void getYMonotonePolygones(){
-		throw new UnsupportedOperationException();
-	}
-	void pol(){
-		throw new UnsupportedOperationException();
-	}
-	void test(){
-		int x = 23;
-		System.out.println(x);
-	}
+
 }
