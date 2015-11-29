4aaedb469f7764603e2de7d6681eb674343f9a64
adiIspas <adi.ispas95@gmail.com>
2015-11-29 15:49:14 +0200

    Add constructor empty and constructor with ArrayList

--- src/util/PoligonMonoton.java
+++ src/util/PoligonMonoton.java
@@ -3,6 +3,13 @@ package util;
 import java.util.ArrayList;
 
 public class PoligonMonoton extends Poligon {
+	
+	public PoligonMonoton(){}
+	
+	public PoligonMonoton(ArrayList<Punct> puncte){
+		super(puncte);
+	}
+	
 	@Override
 	public void makeTriangulare(){
 		throw new UnsupportedOperationException();
