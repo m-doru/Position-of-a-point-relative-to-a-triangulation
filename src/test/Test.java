6864885e44002f89b1e9a72f0711d524cfdee29c
m-doru <doru.musuroi@gmail.com>
2015-12-01 14:46:20 +0200

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.SwingConstants;
+
+import grafica.Frame;
+import grafica.SwingConsole;
+import util.Poligon;
+import util.Punct;
+
+public class Test {
+
+	public static void main(String[] args) {
+		File inputFile = new File("E:\\Triangulari\\src\\test\\default.in");
+		Scanner input;
+		try {
+			input = new Scanner(inputFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		ArrayList<Punct> inputArray = new ArrayList<>();
+		while(input.hasNextDouble()){
+			inputArray.add(new Punct(input.nextDouble(), input.nextDouble()));
+		}
+		Frame frame = new Frame();
+		frame.setPoints(inputArray);
+		SwingConsole.run(frame, 800, 900);
+	}
+
+}
