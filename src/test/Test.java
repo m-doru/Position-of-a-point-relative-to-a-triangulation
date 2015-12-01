5bf9e284f2cfff2794d44432d9e1b95968150c9a
adiIspas <adi.ispas95@gmail.com>
2015-12-01 19:36:45 +0200

    Final Version

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,19 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-import javax.swing.SwingConstants;
-
 import grafica.Frame;
 import grafica.SwingConsole;
-import util.Poligon;
 import util.Punct;
 
 public class Test {
 
 	public static void main(String[] args) {
-		File inputFile = new File("E:\\Triangulari\\src\\test\\default.in");
+		File inputFile = new File("F:\\GitHub\\src\\util\\default.in");
 		Scanner input;
 		try {
 			input = new Scanner(inputFile);
@@ -28,6 +23,8 @@ public class Test {
 		while(input.hasNextDouble()){
 			inputArray.add(new Punct(input.nextDouble(), input.nextDouble()));
 		}
+		
+		input.close();
 		Frame frame = new Frame();
 		frame.setPoints(inputArray);
 		SwingConsole.run(frame, 800, 900);
